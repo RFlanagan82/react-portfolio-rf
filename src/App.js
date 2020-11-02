e35a81fda80f7c1a40bf2235ba3508a6a5076424
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-11-02 16:27:07 -0500

    set up initial file structure and brought in instructions.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+// import {useEffect, useState} from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Nav />
+        <Switch>
+        <Route exact path = "/" component={Home} />
+        <Route exact path = "/About" component={About} />
+        <Route exact path = "/Resume" component={Resume} />
+        <Route exact path = "/Portfolio" component={Portfolio} />
+        <Route exact path = "/Contact" component={Contact} />
+        </Switch>
+      <Footer />
     </div>
+    </Router>
   );
 }
 
