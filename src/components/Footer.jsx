36624f8a071fe58b07bb32c6ec8f54782776f001
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-11-03 06:27:50 -0500

    added jumbotron and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import "../styles/Footer.css";
 function Footer() {
     return (
     <>
-        <footer class="row" id="footer">
-            <p class="footertext text-center">© 2020. Ryan Flanagan - Phone: 770.906.2060
+        <footer className="row" id="footer">
+            <p className="footertext text-center">© 2020. Ryan Flanagan - Phone: 770.906.2060
             </p>
         </footer>
     </>
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "../styles/Jumbotron.css";
+
+function Jumbotron() {
+    return (
+    <>
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <div className="container intro">
+                    <h1 className="display-3">Hello! &lt;My name is Ryan&gt;</h1>
+                        <p className="lead">I create digital experiences that delight and inspire.</p>
+                </div>
+            </div>
+                <p className="skills">HTML5 | CSS3 | JavaScript | Node.js | jQuery | MySQL | MongoDB</p>
+        </div>
+    </>
+    )
+}
+
+export default Jumbotron;
