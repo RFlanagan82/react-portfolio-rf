508da9abfc632538eda8c0d51efb0c6027df7de6
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-12-01 11:26:40 -0500

    added email to footer and additional notes on project.json file

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,9 @@ function Footer() {
     return (
     <>
         <footer className="row" id="footer">
-            <p className="footertext text-center">© 2020. Ryan Flanagan - Phone: 770.906.2060
+            <p className="footertext text-center">© 2020. Ryan Flanagan
+            </p>
+            <p className="footertext text-center">Phone: 770.906.2060 / Email: flanagan.ryan@gmail.com
             </p>
         </footer>
     </>
