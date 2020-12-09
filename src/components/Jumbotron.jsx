a58a76898a798c23c2767bd5a64f092dc2272e8c
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-12-09 15:21:58 -0500

    updated homepage and project.json text for CulTourist

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,18 +3,23 @@ import "../styles/Jumbotron.css";
 
 function Jumbotron() {
     return (
-    <>
+      <>
         <div className="jumbotron jumbotron-fluid">
-            <div className="container">
-                <div className="container intro">
-                    <h1 className="display-3">Hello! &lt;My name is Ryan&gt;</h1>
-                        <p className="lead">I create digital experiences that delight and inspire.</p>
-                </div>
+          <div className="container">
+            <div className="container intro">
+              <h1 className="display-3">Hello! &lt;My name is Ryan&gt;</h1>
+              <p className="lead">
+                I create digital experiences that delight and inspire.
+              </p>
             </div>
-                <p className="skills">HTML5 | CSS3 | JavaScript | Node.js | jQuery | MySQL | MongoDB</p>
+          </div>
+          <p className="skills">
+            HTML5 | CSS3 | JavaScript | React.js | jQuery | MySQL | MongoDB |
+            Node.js
+          </p>
         </div>
-    </>
-    )
+      </>
+    );
 }
 
 export default Jumbotron;
